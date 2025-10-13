7d37c5cfcfede57e8d3d76e0b3e0336085ff1fff
salma <s.shamohammadi98@gmail.com>
2025-10-13 19:44:34 +0330

    list exams-2

---
+++ script.js
@@ -0,0 +1,63 @@
+const ListExam = 
+    [
+        //word
+        [
+            {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+                        {
+                name: 'آزمون 1',
+                description: 'موضوع',
+                link: 'word/index.html'
+            },
+        ],
+        //Excel
+        [
+
+        ]
+    ];
+ 
+ function showModal(index) {
+    debugger
+    ListExam[index].forEach(element => {
+        const newLi = document.createElement("li");
+        newLi.classList.add("my-class", "my-class-2");
+        const newA = document.createElement("a");
+        newA.classList.add("link-exam");
+        newA.innerText = element.name;
+        newA.href = element.link;
+        const newP = document.createElement("p");
+        newP.innerText = element.description;
+        newP.classList.add("my-class");
+        newLi.appendChild(newA);
+        newLi.appendChild(newP);
+        document.getElementById("exams").appendChild(newLi);
+    });
+ }   

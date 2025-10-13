95cff463c67e0397d1833f959fcc4e33a8b12011
salma <s.shamohammadi98@gmail.com>
2025-10-13 21:58:03 +0330

    list exams-5

--- script.js
+++ script.js
@@ -2,44 +2,22 @@ const ListExam =
     [
         //word
         [
-            {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
-                        {
-                name: 'آزمون 1',
-                description: 'موضوع',
-                link: 'word/index.html'
-            },
+            
         ],
         //Excel
         [
+            {
+                number: 'آزمون 15',
+                subject: 'اعتبارسنجی داده های ورودی، Data Validation، معتبرسازی داده های ورودی',
+                //description: 'طراح: مائده قنوی',
+                link: 'Excel/exam-15/story.html'
+            },
+            {
+                number: 'آزمون 18',
+                subject: 'مفهوم ماکرو، تب Developer، مدیریت ماکروهای ضبط شده',
+                //description: 'طراح: مریم محمدخانی',
+                link: 'Excel/exam-18/story.html'
+            }
 
         ],
         //PowerPoint
@@ -68,7 +46,26 @@ const ListExam =
         ],
         //GoogleTools
         [
-
+            {
+                name: 'آزمون 1',
+                description: 'Gmail - ساخت حساب کاربری',
+                link: 'GoogleTools/exam-01/story.html'
+            },
+            {
+                name: 'آزمون 5',
+                description: 'اسناد گوگل (Google Docs)',
+                link: 'GoogleTools/exam-05/story.html'
+            },
+            {
+                name: 'آزمون 6',
+                description: 'جلسات آنلاین (Google Meet)',
+                link: 'GoogleTools/exam-06/story.html'
+            },
+            {
+                name: 'آزمون 7',
+                description: 'فرم آنلاین (Google Forms)',
+                link: 'GoogleTools/exam-07/story.html'
+            }
         ],
         //RepairPc
         [
@@ -97,18 +94,22 @@ const ListExam =
     ];
  
  function showModal(index) {
-    debugger
+    document.getElementById("exams").innerHTML = "";
     ListExam[index].forEach(element => {
         const newLi = document.createElement("li");
-        newLi.classList.add("my-class", "my-class-2");
+        newLi.classList.add("item-exam", "my-class-2");
         const newA = document.createElement("a");
         newA.classList.add("link-exam");
-        newA.innerText = element.name;
+        newA.innerText = element.number;
         newA.href = element.link;
         const newP = document.createElement("p");
+        const newS = document.createElement("p");
+        newS.classList.add("item-subject");
         newP.innerText = element.description;
+        newS.innerText = element.subject;
         newP.classList.add("my-class");
         newLi.appendChild(newA);
+        newLi.appendChild(newS);
         newLi.appendChild(newP);
         document.getElementById("exams").appendChild(newLi);
     });
